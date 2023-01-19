07e985f49a5317996b42fe36ca79ae4c0716664e
Maria <mariababko4292@gmail.com>
2023-01-18 16:00:23 -0800

    Added Earth entity and space background

--- main.js
+++ main.js
@@ -2,9 +2,16 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./space_background.png");
+ASSET_MANAGER.queueDownload("./earth.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Earth(gameEngine));
 
 	gameEngine.init(ctx);
 
