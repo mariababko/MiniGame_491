72475232ebfb25808bf5328b0ad8b90d52d478f6
Maria <mariababko4292@gmail.com>
2023-01-18 21:18:18 -0800

    Added shooting area and animated asteriod and asteriod entities

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("./space_background.png");
 ASSET_MANAGER.queueDownload("./earth.png");
+ASSET_MANAGER.queueDownload("./asteriod1.png");
+ASSET_MANAGER.queueDownload("./asteriod2.png");
+ASSET_MANAGER.queueDownload("./asteriod3.png");
+ASSET_MANAGER.queueDownload("./shooting_area.png");
+ASSET_MANAGER.queueDownload("./explosion.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -11,7 +16,17 @@ ASSET_MANAGER.downloadAll(() => {
 
 	ctx.imageSmoothingEnabled = false;
 
+	gameEngine.addEntity(new Shooting_Area(gameEngine));
+
+	gameEngine.addEntity(new Asteriod(gameEngine));
+
+	setTimeout(() => { gameEngine.addEntity(new Asteriod(gameEngine)); }, 2000);
+
+	setTimeout(() => { gameEngine.addEntity(new Asteriod(gameEngine)); }, 3000);
+
 	gameEngine.addEntity(new Earth(gameEngine));
+
+	//gameEngine.addEntity(new Explosion(gameEngine));
 
 	gameEngine.init(ctx);
 
